ceaa4d5f11a642011b72d01635571a8cec5d8322
Ragaie alfy <Ragaie@ehabs-bc-macbook-pro.local>
2017-11-22 14:55:56 +0200

    add new method to delegate

--- CheckBox/ViewController.swift
+++ CheckBox/ViewController.swift
@@ -49,7 +49,7 @@ class ViewController: UIViewController ,CheckBoxDelegate,RadioGroupDelegate{
         
         
         
-        radioGroup.selectIndex
+        print(radioGroup.selectIndex)
     }
     
     
